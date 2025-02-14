f064ca18e7da1a527feaa7876200667410e24699
mymindinsane <arturguletskiy21@gmail.com>
2025-02-14 16:28:13 +0300

    done

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -2,7 +2,7 @@ package koschei.models;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Deth8 {
 
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
-    @Autowired
-    private Deth8 deth8;
-
-
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( ";
